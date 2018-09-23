0dcbfcbcb766044e92d2a31b8f3fad7ec1b55bee
anandhupvr <anandhupvr@gmail.com>
2018-09-23 18:50:15 +0530

    archi

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from matplotlib import pyplot as plt
 import numpy as np
-from network.darknet import Arch
+from network.net import Anet
 import config.parameters as p
 from loss import loss
 import cv2
@@ -10,14 +10,11 @@ from utils import datamaker
 
 config = p.getParams()
 images, labels = datamaker.get_data(config)
-
-arch = Arch(config)
-
-preds = arch.darknet()
+arch = Anet(config)
+preds = arch.network()
 x = arch.getX()
-
 with tf.Session() as sess:
-	loss = loss.yolo_loss(preds, config, labels)
-	train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
-	sess.run(tf.global_variables_initializer())
-	sess.run(train_step, feed_dict={x:images})
+    loss = loss.yolo_loss(preds, config, labels)
+    train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
+    sess.run(tf.global_variables_initializer())
+    sess.run(train_step, feed_dict={x:images})
