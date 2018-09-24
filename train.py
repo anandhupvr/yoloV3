df2cc06c661a542cbdc23d2c722c1f0b47226f9a
food-vision <anandhupvr@gmail.com>
2018-09-24 17:37:00 +0530

    nohting

--- train.py
+++ train.py
@@ -13,12 +13,9 @@ images, labels = datamaker.get_data(config)
 arch = Anet(config)
 preds = arch.network()
 x = arch.getX()
-
-
-
 with tf.Session() as sess:
     loss = loss.yolo_loss(preds, config, labels)
     train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
     sess.run(tf.global_variables_initializer())
     sess.run(train_step, feed_dict={x:images})
-    writer =  tf.summary.FileWriter("/run/media/anandhu/baskaran/agrima/git_repos/yoloV2", sess.graph)
+    # writer =  tf.summary.FileWriter("/run/media/anandhu/baskaran/agrima/git_repos/yoloV2", sess.graph)
