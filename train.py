e88602965571b1c30154dd1bfd9f706bff528539
food-vision <anandhupvr@gmail.com>
2018-09-27 01:55:29 +0530

    nnn

--- train.py
+++ train.py
@@ -16,9 +16,13 @@ arch = Anet(config)
 
 preds = arch.network()
 x = arch.getX()
+epochs = 10
 
 with tf.Session() as sess:
-	loss = loss.yolo_loss(preds, config, labels)
-	train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
-	sess.run(tf.global_variables_initializer())
-	sess.run(train_step, feed_dict={x:images})
+	for epoch in range(epochs):
+		print ("epoch : "+str(epoch))
+		loss = loss.yolo_loss(preds, config, labels)
+		train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
+		sess.run(tf.global_variables_initializer())
+		sess.run(train_step, feed_dict={x:images})
+		print("total loss : "+str(loss))
