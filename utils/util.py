412302c6448c29fd877af2bcaa00616d9d2c8ada
anandhupvr <anandhupvr@gmail.com>
2023-08-04 10:21:17 +0800

    nms, cells_to_boxes

--- utils/util.py
+++ utils/util.py
@@ -1,4 +1,14 @@
 import torch
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy as np
+
+COCO_LABELS = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 
+'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard',
+ 'sports ball', 'kite',  'baseball bat',  'baseball glove',  'skateboard',  'surfboard',  'tennis racket',  'bottle',  'wine glass',  'cup',  'fork',  'knife',  'spoon',
+ 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+ 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+]
 
 # Defining a function to calculate Intersection over Union (IoU)
 def iou(box1, box2, is_pred=True):
@@ -56,6 +66,123 @@ def iou(box1, box2, is_pred=True):
         # Return IoU score
         return iou_score
 
+
+
+# Non-maximum suppression function to remove overlapping bounding boxes
+def nms(bboxes, iou_threshold, threshold):
+    # Filter out bounding boxes with confidence below the threshold.
+    bboxes = [box for box in bboxes if box[1] > threshold]
+  
+    # Sort the bounding boxes by confidence in descending order.
+    bboxes = sorted(bboxes, key=lambda x: x[1], reverse=True)
+  
+    # Initialize the list of bounding boxes after non-maximum suppression.
+    bboxes_nms = []
+  
+    while bboxes:
+        # Get the first bounding box.
+        first_box = bboxes.pop(0)
+  
+        # Iterate over the remaining bounding boxes.
+        for box in bboxes:
+        # If the bounding boxes do not overlap or if the first bounding box has
+        # a higher confidence, then add the second bounding box to the list of
+        # bounding boxes after non-maximum suppression.
+            if box[0] != first_box[0] or iou(
+                torch.tensor(first_box[2:]),
+                torch.tensor(box[2:]),
+            ) < iou_threshold:
+                # Check if box is not in bboxes_nms
+                if box not in bboxes_nms:
+                    # Add box to bboxes_nms
+                    bboxes_nms.append(box)
+  
+    # Return bounding boxes after non-maximum suppression.
+    return bboxes_nms
+
+
+def cells_to_bboxes(predictions, anchors, S, is_preds=True):
+    """
+    Scales the predictions coming from the model to
+    be relative to the entire image such that they for example later
+    can be plotted or.
+    INPUT:
+    predictions: tensor of size (N, 3, S, S, num_classes+5)
+    anchors: the anchors used for the predictions
+    S: the number of cells the image is divided in on the width (and height)
+    is_preds: whether the input is predictions or the true bounding boxes
+    OUTPUT:
+    converted_bboxes: the converted boxes of sizes (N, num_anchors, S, S, 1+5) with class index,
+                      object score, bounding box coordinates
+    """
+    BATCH_SIZE = predictions.shape[0]
+    num_anchors = len(anchors)
+    box_predictions = predictions[..., 1:5]
+    if is_preds:
+        anchors = anchors.reshape(1, len(anchors), 1, 1, 2)
+        box_predictions[..., 0:2] = torch.sigmoid(box_predictions[..., 0:2])
+        box_predictions[..., 2:] = torch.exp(box_predictions[..., 2:]) * anchors
+        scores = torch.sigmoid(predictions[..., 0:1])
+        best_class = torch.argmax(predictions[..., 5:], dim=-1).unsqueeze(-1)
+    else:
+        scores = predictions[..., 0:1]
+        best_class = predictions[..., 5:6]
+
+    cell_indices = (
+        torch.arange(S)
+        .repeat(predictions.shape[0], 3, S, 1)
+        .unsqueeze(-1)
+        .to(predictions.device)
+    )
+    x = 1 / S * (box_predictions[..., 0:1] + cell_indices)
+    y = 1 / S * (box_predictions[..., 1:2] + cell_indices.permute(0, 1, 3, 2, 4))
+    w_h = 1 / S * box_predictions[..., 2:4]
+    converted_bboxes = torch.cat((best_class, scores, x, y, w_h), dim=-1).reshape(BATCH_SIZE, num_anchors * S * S, 6)
+    return converted_bboxes.tolist()
+
+def plot_image(image, boxes):
+    """Plots predicted bounding boxes on the image"""
+    cmap = plt.get_cmap("tab20b")
+    class_labels = COCO_LABELS
+    colors = [cmap(i) for i in np.linspace(0, 1, len(class_labels))]
+    im = np.array(image)
+    height, width, _ = im.shape
+
+    # Create figure and axes
+    fig, ax = plt.subplots(1)
+    # Display the image
+    ax.imshow(im)
+
+    # box[0] is x midpoint, box[2] is width
+    # box[1] is y midpoint, box[3] is height
+
+    # Create a Rectangle patch
+    for box in boxes:
+        assert len(box) == 6, "box should contain class pred, confidence, x, y, width, height"
+        class_pred = box[0]
+        box = box[2:]
+        upper_left_x = box[0] - box[2] / 2
+        upper_left_y = box[1] - box[3] / 2
+        rect = patches.Rectangle(
+            (upper_left_x * width, upper_left_y * height),
+            box[2] * width,
+            box[3] * height,
+            linewidth=2,
+            edgecolor=colors[int(class_pred)],
+            facecolor="none",
+        )
+        # Add the patch to the Axes
+        ax.add_patch(rect)
+        plt.text(
+            upper_left_x * width,
+            upper_left_y * height,
+            s=class_labels[int(class_pred)],
+            color="white",
+            verticalalignment="top",
+            bbox={"color": colors[int(class_pred)], "pad": 0},
+        )
+
+    plt.show()
 
 def test():
     target = torch.tensor([[2.5, 2, 3, 2], [2.5, 2, 3, 2]])
